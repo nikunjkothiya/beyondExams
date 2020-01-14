bb0850879cb908dc2ae49d8794561900764344be
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2020-01-14 13:27:56 +0530

    New APIs for React

--- app/Http/Controllers/PreciselyController.php
+++ app/Http/Controllers/PreciselyController.php
@@ -3,56 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Country;
+use App\DeveloperDetail;
 use App\Discipline;
 use App\Language;
 use App\Qualification;
 use App\Tag;
 use App\UserDetail;
+use Config;
 use Illuminate\Http\Request;
-use App\Http\Controllers\ApiResponse;
-use App\User;
-use App\DeveloperDetail;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 /**
- * @property \App\Http\Controllers\ApiResponse apiResponse
+ * @property ApiResponse apiResponse
  */
 class PreciselyController extends Controller
 {
-    public function __construct(ApiResponse $apiResponse){
-        $this->msg="";
-        $this->apiResponse=$apiResponse;
+    public function __construct(ApiResponse $apiResponse)
+    {
+        $this->msg = "";
+        $this->apiResponse = $apiResponse;
     }
 
-    public function get_language(Request $request) {
-        try{
-            $languages = DB::table('languages')->select('code', 'language','language_example')->get();
+    public function get_language(Request $request)
+    {
+        try {
+            $languages = DB::table('languages')->select('code', 'language', 'language_example')->get();
 
-            return $this->apiResponse->sendResponse(200,'All languages fetched successfully', $languages);
-        }
-        catch(Exception $e){
-            return $this->apiResponse->sendResponse(500,'Internal Server Error', $e);
+            return $this->apiResponse->sendResponse(200, 'All languages fetched successfully', $languages);
+        } catch (Exception $e) {
+            return $this->apiResponse->sendResponse(500, 'Internal Server Error', $e);
         }
     }
 
 
-    public function get_filters(Request $request) {
-        try{
+    public function get_filters(Request $request)
+    {
+        try {
             $filters = Tag::all();
 
-            return $this->apiResponse->sendResponse(200,'All languages fetched successfully', $filters);
-        }
-        catch(Exception $e){
-            return $this->apiResponse->sendResponse(500,'Internal Server Error', $e);
+            return $this->apiResponse->sendResponse(200, 'All languages fetched successfully', $filters);
+        } catch (Exception $e) {
+            return $this->apiResponse->sendResponse(500, 'Internal Server Error', $e);
         }
     }
 
     public function submit_profile(Request $request)
     {
         try {
-                $validator = Validator::make($request->all(), [
+            $validator = Validator::make($request->all(), [
                 'firstname' => 'required|string|max:255',
                 'lastname' => 'required|string|max:255',
                 'college' => 'required|string|max:1024',
@@ -65,16 +64,18 @@ class PreciselyController extends Controller
             ]);
 //            dd($validator);
 
-            if($validator->fails()){
-                return $this->apiResponse->sendResponse(400,'Parameters missing or invalid.',$validator->errors());
+            if ($validator->fails()) {
+                return $this->apiResponse->sendResponse(400, 'Parameters missing or invalid.', $validator->errors());
             }
 
-            $check = UserDetail::where('user_id', $request->id)->first();
+            $user_id = $request->user()->id;
+
+            $check = UserDetail::where('user_id', $user_id)->first();
 
             if (is_null($check)) {
                 $record = new UserDetail;
-                $record->user_id = $request->id;
-                $record->language_id = Language::where('code', \Config::get('app.locale'))->first()->id;
+                $record->user_id = $user_id;
+                $record->language_id = $request->language_code;
                 $record->email = $request->email;
                 $record->firstname = $request->firstname;
                 $record->lastname = $request->lastname;
@@ -86,12 +87,11 @@ class PreciselyController extends Controller
                 $record->country_id = $request->country;
                 $record->save();
                 if ($record) {
-                    return $this->apiResponse->sendResponse(200,'User details saved.', $record);
+                    return $this->apiResponse->sendResponse(200, 'User details saved.', $record);
                 } else {
-                    return $this->apiResponse->sendResponse(500,'Internal server error. New record could not be inserted', null);
+                    return $this->apiResponse->sendResponse(500, 'Internal server error. New record could not be inserted', null);
                 }
             } else {
-                $check->language_id = Language::where('code', \Config::get('app.locale'))->first()->id;
                 $check->email = $request->email;
                 $check->firstname = $request->firstname;
                 $check->lastname = $request->lastname;
@@ -103,13 +103,111 @@ class PreciselyController extends Controller
                 $check->country_id = $request->country;
                 $check->save();
                 if ($check) {
-                    return $this->apiResponse->sendResponse(200,'User details saved.', $check);
+                    return $this->apiResponse->sendResponse(200, 'User details saved.', $check);
                 } else {
-                    return $this->apiResponse->sendResponse(500,'Internal server error. Record could not be updated', null);
+                    return $this->apiResponse->sendResponse(500, 'Internal server error. Record could not be updated', null);
                 }
             }
         } catch (Exception $e) {
-            return $this->apiResponse->sendResponse(500,'Internal server error 3.', $e->getMessage());
+            return $this->apiResponse->sendResponse(500, 'Internal server error 3.', $e->getMessage());
+        }
+    }
+
+//    TODO: CORRECT RETURN TYPE
+    public function get_profile(Request $request){
+        try{
+            $pcheck = UserDetail::where('user_id',$request->user()->id)->first();
+        }
+        catch(Exception $e){
+            return $this->apiResponse->sendResponse(500, 'User authentication failed', $e->getMessage());
+        }
+
+        if($pcheck){
+            $countries = Country::all();
+            $disciplines = Discipline::all();
+            $qualifications = Qualification::all();
+            return view('pages.profile',['languages'=>$this->languages,'pcheck'=>$pcheck,'countries'=>$countries,'disciplines'=>$disciplines,'qualifications'=>$qualifications,'txnflag'=>$this->txnflag->check_subscription(Auth::user()->id)]);
+        }
+        return redirect('/setup/2');
+
+    }
+
+    public function save_opportunity(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required|exists:opportunities',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->apiResponse->sendResponse(400, 'Parameters missing or invalid.', $validator->errors());
+            }
+
+            $id = $request->id;
+            $user = $request->user();
+            $user->saved_opportunities()->detach($id);
+            $user->saved_opportunities()->attach($id);
+        } catch (Exception $e) {
+            return $this->apiResponse->sendResponse(500, 'Internal server error.', $e->getMessage());
+        }
+        return $this->apiResponse->sendResponse(200, 'Opportunity saved', null);
+    }
+
+    public function unsave_opportunity(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required|exists:opportunities',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->apiResponse->sendResponse(400, 'Parameters missing or invalid.', $validator->errors());
+            }
+
+            $id = $request->id;
+            $user = $request->user();
+            $user->saved_opportunities()->detach($id);
+        } catch (Exception $e) {
+            return $this->apiResponse->sendResponse(500, 'Internal server error.', $e->getMessage());
+        }
+
+        return $this->apiResponse->sendResponse(200, 'Opportunity removed from saved', null);
+    }
+
+    public function save_user_language(Request $request) {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'language_id' => 'required|exists:languages',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->apiResponse->sendResponse(400, 'Parameters missing or invalid.', $validator->errors());
+            }
+
+
+            $user = $request->user();
+            $check = UserDetail::where('user_id', $user->id)->first();
+            $check->language_id = $request->language_id;
+
+
+        } catch (Exception $e) {
+            return $this->apiResponse->sendResponse(500, 'Internal server error.', $e->getMessage());
+        }
+    }
+
+    public function save_user_filter(Request $request){
+        try{
+            $user = $request->user();
+            $tags = $request->tags;
+            if(empty($tags)){
+                return $this->apiResponse->sendResponse(400, 'Select at least one filter', null);
+            }
+            $user->tags()->sync($tags);
+            return $this->apiResponse->sendResponse(200, 'Saved filters selected by user', null);
+        }
+        catch(Exception $e){
+            return $this->apiResponse->sendResponse(500, 'Internal server error.', $e->getMessage());
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,16 @@ Route::post('{provider}/verifyAccessToken',['uses'=>'ApiAuthController@verifyAcc
 Route::post('refresh',['uses'=>'ApiAuthController@refresh']);
 Route::get('get_all_languages',['uses'=>'PreciselyController@get_language']);
 Route::get('get_filters',['uses'=>'PreciselyController@get_filters']);
-Route::post('submit_user_profile',['uses'=>'PreciselyController@submit_profile']);
 
 //Protected APIs
 Route::group(['middleware' => 'auth:api'], function(){
 	Route::post('logout',['uses'=>'ApiAuthController@logout']);
+    Route::post('submit_user_profile',['uses'=>'PreciselyController@submit_profile']);
+    Route::post('get_profile',['uses'=>'PreciselyController@get_profile']);
+    Route::post('save_user_filter',['uses'=>'PreciselyController@save_user_filter']);
+    Route::post('save_user_language',['uses'=>'PreciselyController@save_user_language']);
+    Route::post('unsave_opportunity',['uses'=>'PreciselyController@unsave_opportunity']);
+    Route::post('save_opportunity',['uses'=>'PreciselyController@save_opportunity']);
 });
 
 
