70e0d7e92c30f89f8cb25910c6bebd48d8d5a0db
Ravish <ravish1729@gmail.com>
2020-02-28 22:17:49 +0530

    Authentication Adding Cookies

--- app/Http/Controllers/ApiResponse.php
+++ app/Http/Controllers/ApiResponse.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class ApiResponse extends Controller
 {
     public function sendResponse($code,$message,$data){
     	if($code == 200 || $code == 201){
+			if (array_key_exists("token", $data)){
+				$cookie = Cookie::make('access_token', $data["token"], null, null, null, false, false);
+				return response([
+					'status' => 'success',
+					'status_code' => $code,
+					'message' => $message,
+					'data' => $data
+				],$code)->withCookie($cookie);
+			}
     		return response([
     			'status' => 'success',
     			'status_code' => $code,
