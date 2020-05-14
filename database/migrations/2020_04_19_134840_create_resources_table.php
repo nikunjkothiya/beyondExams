b14cdfbd5f31eef3edd0ddfeb2c97a178e5b2201
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2020-05-14 20:49:59 +0530

    Tables for resources

---
+++ database/migrations/2020_04_19_134840_create_resources_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+//        private $file_parameters = ["url", "thumbnail", "type", "length", "title", "author", "designation", "profile_pic"];
+        Schema::create('resources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('file_url');
+            $table->string('thumbnail_url')->nullable();
+            $table->unsignedInteger('file_type_id');
+            $table->integer('duration')->nullable();
+            $table->string('title');
+            $table->unsignedBigInteger('author_id');
+            $table->timestamps();
+        });
+
+        Schema::table('resources',function($table){
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('file_type_id')->references('id')->on('file_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
