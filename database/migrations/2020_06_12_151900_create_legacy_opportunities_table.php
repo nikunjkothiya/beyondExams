c581edd55c74168337dec8ee20bfcc368a6258fc
Nikhil Kadyan <nikhilkadyan@icloud.com>
2020-06-18 13:53:42 +0530

    Master Merge

--- database/migrations/2020_06_12_151900_create_legacy_opportunities_table.php
+++ database/migrations/2020_06_12_151900_create_legacy_opportunities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLegacyOpportunityTable extends Migration
+class CreateLegacyOpportunitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -31,6 +31,6 @@ class CreateLegacyOpportunityTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('legacy_opportunity');
+        Schema::dropIfExists('legacy_opportunities');
     }
 }
