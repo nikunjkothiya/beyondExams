977cc9e42c8c68bea334f83763c9e7a5f149916a
hitesh <hitesh@precisely.co.in>
2021-03-18 04:34:32 +0000

    Whatsapp file path bug fix

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -219,6 +219,7 @@ class ChatController extends Controller
         try {
             $validator = Validator::make($request->all(), [
                 'chat_id' => 'required|integer',
+		'folder_name' => 'required|string'
             ]);
 
             if ($validator->fails()) {
@@ -227,13 +228,14 @@ class ChatController extends Controller
 
             $chat = Chat::find($request->chat_id);
             if (!is_null($chat)) {
-                $name = $chat->title;
+                $name = $request->folder_name;
 
                 $old_message = ChatMessage::where(['chat_id' => $chat->id])->where('type_id','!=',1)->get();
                 foreach($old_message as $message){
-                    $change_path = ChatMessage::where(['chat_id' => $chat->id,'id'=>$message->id])->where('type_id','!=',1)->first();
-                    $change_path->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$name."/", $message->message);
-                    $change_path->save();
+//                    $change_path = ChatMessage::where(['chat_id' => $chat->id,'id'=>$message->id])->where('type_id','!=',1)->first();
+                    $message->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$name."/", $message->message);
+		    $message->message = "https://api.learnwithyoutube.org/" . $message->message;
+                    $message->save();
                 }
                 DB::commit();
                 return $this->apiResponse->sendResponse(200, 'Successfully Changed Filepaths', null);
@@ -310,15 +312,19 @@ class ChatController extends Controller
                             } elseif ($value[3] == 'Photo') {
                                 $chat_message->type_id = 2;
                                 $chat_message->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$request->chat_name."/", $value[2]);
+				$chat_message->message = "https://api.learnwithyoutube.org/" . $chat_message->message;
                             } elseif ($value[3] == 'Video') {
                                 $chat_message->type_id = 3;
                                 $chat_message->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$request->chat_name."/", $value[2]);
+				$chat_message->message = "https://api.learnwithyoutube.org/" . $chat_message->message;
                             } elseif ($value[3] == 'Audio') {
                                 $chat_message->type_id = 4;
                                 $chat_message->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$request->chat_name."/", $value[2]);
+				$chat_message->message = "https://api.learnwithyoutube.org/" . $chat_message->message;
                             } elseif ($value[3] == 'File') {
                                 $chat_message->type_id = 5;
                                 $chat_message->message = str_replace("WhatsApp-Scraping/","classroom_assets/".$request->chat_name."/", $value[2]);
+				$chat_message->message = "https://api.learnwithyoutube.org/" . $chat_message->message;
                             } else {
                                 $chat_message->type_id = 1;
                                 $chat_message->message = $value[2];
